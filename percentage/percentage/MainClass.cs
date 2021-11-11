dcf3a79ddddc060779b6ef323fd9778988f32b39
Your Name <you@example.com>
2021-11-11 19:18:18 +0530

    Calculate the percentage for an exam of a student using interface impliment for SSC,NIT & IIT exams

---
+++ percentage/percentage/MainClass.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace percentage
+{
+    class MainClass
+    {
+        static void Main(string[] args)
+        {
+            int choice;
+          
+            do {
+               
+                Console.WriteLine("Please select which exam student had given?:");
+                Console.WriteLine("1.SSC");
+                Console.WriteLine("2.NIT");
+                Console.WriteLine("3.IIT");
+                Console.WriteLine("0.Exit");
+                Console.Write("Please enter a choice:");
+                choice = Convert.ToInt32(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        SSC ssc = new SSC();
+                        ssc.getpercentage();
+                        break;
+                    case 2:
+                        NIT nit = new NIT();
+                        nit.getpercentage();
+                        break;
+                    case 3:
+                        IIT iit = new IIT();
+                        iit.getpercentage();
+                        break;
+                    default:
+                        Console.WriteLine("Invalid choice");
+                        break;
+                }
+            } while (choice!=0);
+           
+        }
+    }
+}
