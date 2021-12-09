e5d8559a5d1381fe677b46e54f4d1cefe545bb42
Abhishek Anil Dhekane <87105368+Abhi7858@users.noreply.github.com>
2021-12-09 13:43:33 +0530

    Update Payroll.cs

--- OfficeApplication/OfficeApplication/Payroll.cs
+++ OfficeApplication/OfficeApplication/Payroll.cs
@@ -4,10 +4,10 @@ namespace OfficeApplication
 {
     public class Payroll : Department
     {
-        public void UpdateSalary(int updateSalaryId)
+        public void UpdateSalary(int employeeId)
         {
             var Context = new EmployeeEntities();
-            var Employee = Context.TblEmployees.Find(updateSalaryId);
+            var Employee = Context.TblEmployees.Find(employeeId);
             if (Employee != null)
             {
                 Console.Write("Update Salary of {0}: ", Employee.EmployeeName);
