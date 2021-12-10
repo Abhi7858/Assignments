b25cb89468475aa7351d9f94feda4bad47d5ecc9
Abhishek Anil Dhekane <87105368+Abhi7858@users.noreply.github.com>
2021-12-10 12:49:38 +0530

    Update Program.cs

--- reverse/reverse/Program.cs
+++ reverse/reverse/Program.cs
@@ -1,33 +1,21 @@
-﻿/*
-Problem Statement:- How to reverse the order of words in a given string?
-input:- Welcome to Csharp corner
-output:- corner Csharp to Welcome
- */
-
 using System;
 
-namespace reverse
+namespace ReverseOrderOfWords
 
 {
     class Reverse
 
     {
         static void Main()
-
         {
             Console.Write("input String:");
-            string inp = Console.ReadLine();  // Takes input string from user.
-            string[] arr = inp.Split(' '); // The string is splitted i.e converts into substring and stored in string array. 
-            Array.Reverse(arr); // Reverse method Reverses the order of the elements and stores in that array implicitly.
+            string inputString = Console.ReadLine();  
+            string[] splitArray = inputString.Split(' '); 
+            Array.Reverse(splitArray); 
             Console.Write("Output:");
 
-            foreach (string str in arr)  // prints each substring from reversed string array
-            {
-                Console.Write(str + " ");
-            }
-
+            foreach (string str in splitArray)  
+            Console.Write(str + " ");
         }
-
     }
-
 }
